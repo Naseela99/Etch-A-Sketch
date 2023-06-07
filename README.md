37eac80480d68b0e7f3274eed4ef0665745173b0
Naseela99 <perveznaseela@gmail.com>
2023-06-07 23:52:46 +0530

    add working code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Etch-A-Sketch
+
+This project is a part of [The Odin Project]{https://www.theodinproject.com/dashboard}.
+
+It is a sketching web application, that let's us set the grid size and choose colors to created a pixelated sketches. The ```MultiColor``` mode creates sketches with a random color for every pixel.
+
+The skills used in this project are:
+1. HTML
+2. CSS
+3. JavaScript
--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,11 @@ control_div.appendChild(range_val);
 value  = inp_range.value;
 range_val.textContent = `Grid: ${value} x ${value}`;
 
+let grid = document.querySelector('.grid')
+
+let clicked = false;
+
+
 makeGrid(value)
 
 
@@ -22,29 +27,168 @@ function changeInput(){
 
 }
 
+
+
+// Get color
+let color = "#2d2c27"
+let cur_btn = "Color"
+
+let btns = document.querySelectorAll('.ctrl-btn')
+
+let color_picker = document.querySelector('input[type="color"]');
+
+color_picker.addEventListener("input",updatePickerColor);
+
+
+color_picker.addEventListener("change",updatePickerColor);
+
+function updatePickerColor(){
+    color = color_picker.value
+    clicked = false;
+
+}
+
+
+btns.forEach(function(item) {item.addEventListener('click',changeColor)   
+});
+
+btns.forEach(btn => btn.addEventListener('transitionend',removeTransition))
+
+function removeTransition(e){
+
+    e.target.classList.remove('transform')
+
+}
+function changeColor(e){
+
+    e.target.classList.add('transform')
+
+    let btn = (e.target.textContent)
+    clicked = false;
+
+    if(btn=='Eraser'){
+        color = "white";
+        cur_btn = 'Eraser'
+    }
+
+    else if(btn=='MultiColor'){
+        color = getRandomColor();
+        cur_btn = "MultiColor"
+    }
+
+    else if(btn==='Color'){
+        
+
+        color = color_picker.value
+        cur_btn = "Color"
+    }
+
+    
+}
+
+function getRandomColor(){
+
+    return '#' + (Math.random().toString(16) + "000000").substring(2,8)
+        
+}
+
 // Make Grid
-grid = document.querySelector('.grid')
 
 function makeGrid(val){
+
+    while (grid.firstChild) {
+        grid.firstChild.remove()
+    }
+    
     for(let i=0;i<val;i++){
         d = document.createElement('div');
         
         d.style.display = 'flex';
 
+
         for(let j=0;j<val;j++){
             
             d1 = document.createElement('div')
+            d1.className = "grid-box"
             d1.style.flex=1;
 
             d.appendChild(d1)
             
         }
         d.style.flex=1;
+        d.style.height = Math.round(700/val);
+
         grid.appendChild(d)
+    }
+
+    setListensers()
+}
+
+
+function setListensers(){
+
+    let grid_boxes = document.querySelectorAll('.grid-box')
+    
+    grid_boxes.forEach(function(item) {item.addEventListener('click',start)   
+    });
+
+    grid_boxes.forEach(function(item) {item.addEventListener('mouseover',startPaint)   
+    });
+
+}
+
+function start(){
+
+    if(clicked){
+        clicked = false;
+    }
+    else{
+
+        clicked = true;
+    }
+
+    
+
+}
+
+function startPaint(e){
+    if(clicked){
+        paint(e);
     }
 }
 
-console.log(grid)
+function paint(e){
+
+    if (cur_btn=='MultiColor'){
+        color = getRandomColor();
+    }
+
+    e.target.style.backgroundColor = color;
+
+}
+
+
+let clear = document.querySelector('.clr-btn')
+
+console.log(clear)
+
+clear.addEventListener("click",resetGrid);
+clear.addEventListener('transitionend',removeTransition)
+
+
+function resetGrid(e)
+{
+    let grid_boxes = document.querySelectorAll('.grid-box')
+
+    grid_boxes.forEach(function(item) {item.style.backgroundColor='white';   
+    });
+
+
+e.target.classList.add('transform')
+    
+
+}
+// console.log(grid_boxes)
 
 
 
